5240db7c2dcaafb366483815177b3173d21e1c43
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-05 03:42:50 +0530

    feat: add providers component to wrap the body (trpc and session)

---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import { TRPCProvider } from "@/trpc/client";
+
+export const Providers = ({ children }: { children: ReactNode }) => {
+	return (
+		<SessionProvider>
+			<TRPCProvider>{children}</TRPCProvider>
+		</SessionProvider>
+	);
+};
+// all the providers will go here
