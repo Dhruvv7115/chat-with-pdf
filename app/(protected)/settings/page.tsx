e22baabdf9ca8fd8a4e97cf4a124e769e447ff30
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-11 19:56:34 +0530

    feat: placeholder settings and billings page

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,8 @@
+import { SidebarInset } from "@/components/ui/sidebar";
+import React from "react";
+
+const SettingsPage = () => {
+	return <SidebarInset>SettingsPage</SidebarInset>;
+};
+
+export default SettingsPage;
