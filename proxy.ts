9adf9b297852e441989edf7e08fd7b14426bf51e
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-11 19:51:43 +0530

    fix: proxy fixed

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,5 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 export { default } from "next-auth/middleware";
 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-	return NextResponse.redirect(new URL("/login", request.url));
-}
-
-// Alternatively, you can use a default export:
-// export default function proxy(request: NextRequest) { ... }
-
-export const config = { matcher: ["/dashboard/:path*"] };
+export const config = {
+	matcher: ["/dashboard", "/chat", "/settings", "/billings", "/api/:path*"],
+};
