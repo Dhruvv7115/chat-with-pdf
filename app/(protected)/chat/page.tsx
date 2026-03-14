2ebffbff10f4fd19973fdd80c136269af3164fe9
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-14 13:32:07 +0530

    feat: chat page ui + functionality

--- app/(protected)/chat/page.tsx
+++ app/(protected)/chat/page.tsx
@@ -1,24 +1,84 @@
+"use client";
 import FileUploadDemo from "@/components/file-upload-demo";
 import React from "react";
-import {
-	InputGroup,
-	InputGroupAddon,
-	InputGroupButton,
-	InputGroupInput,
-	InputGroupText,
-	InputGroupTextarea,
-} from "@/components/ui/input-group";
-import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select";
 import ChatInput from "@/components/chat-input";
+import { api } from "@/trpc/client";
+import { toast } from "sonner";
+import { create } from "node:domain";
+import { useRouter } from "next/navigation";
 
 const ChatPage = () => {
+	const uploadPdf = api.pdf.getUploadUrl.useMutation();
+	const savePdf = api.pdf.savePdf.useMutation();
+	const createChat = api.chat.createChat.useMutation();
+	const router = useRouter();
+
+	// await Promise.all(
+	// 	files.map(async (file) => {
+	// 		await createPdf.mutateAsync(
+	// 			{
+	// 				name: file.name,
+	// 				type: file.type,
+	// 				size: file.size,
+	// 			},
+	// 			{
+	// 				onSuccess: (data) => {
+	// 					fetch(data.url, {
+	// 						method: "PUT",
+	// 						body: file,
+	// 					}).then(() => {
+	// 						toast.success("File uploaded successfully");
+	// 					});
+	// 				},
+	// 				onError: (error) => {
+	// 					toast.error("Something went wrong");
+	// 					console.log(error);
+	// 				},
+	// 			},
+	// 		);
+	// 	}),
+	// );
+	const handlePdfUpload = async (files: File[]) => {
+		if (!files.length) return;
+		if (files.length > 1) {
+			toast.error("Only one file at a time is allowed");
+			return;
+		}
+
+		const file = files[0];
+
+		try {
+			// 1. Get pre-signed upload URL
+			const { url, key } = await uploadPdf.mutateAsync({
+				name: file.name,
+				type: file.type,
+				size: file.size,
+			});
+
+			const safeFileName = file.name.replace(/[^a-zA-Z0-9._-]/g, "_");
+
+			// 2. Upload directly to S3
+			await fetch(url, { method: "PUT", body: file });
+
+			// 3. Save PDF to DB
+			const pdf = await savePdf.mutateAsync({
+				key,
+				title: safeFileName,
+			});
+
+			// 4. Create chat
+			const chat = await createChat.mutateAsync({
+				title: safeFileName,
+				pdfId: pdf.id,
+			});
+
+			toast.success("Chat created successfully");
+			router.push(`/chat/${chat.id}`);
+		} catch (error) {
+			toast.error("Something went wrong");
+			console.error(error);
+		}
+	};
 	return (
 		<main className="p-4 flex flex-col items-center justify-start gap-6 w-full h-full">
 			<div className="mb-6">
@@ -31,7 +91,7 @@ const ChatPage = () => {
 			</div>
 
 			<div className="flex gap-2 w-full max-w-4xl mx-auto">
-				<FileUploadDemo />
+				<FileUploadDemo onUpload={handlePdfUpload} />
 				<ChatInput />
 			</div>
 		</main>
