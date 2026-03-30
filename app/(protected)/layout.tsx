f3b64be11384cf2063af6810a23419fed7f4bb8f
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-30 21:56:09 +0530

    fix: SidebarInset height in protected layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,15 +6,13 @@ import {
 	SidebarProvider,
 	SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { TRPCProvider } from "@/trpc/client";
-import { SessionProvider } from "next-auth/react";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
-			<SidebarInset className="h-screen flex flex-col">
+			<SidebarInset className="h-[calc(100vh-1rem)] flex flex-col rounded-xl">
 				<header className="flex h-16 shrink-0 items-center gap-2 border-b border-neutral-200 dark:border-neutral-800">
 					<div className="flex items-center gap-2 px-4">
 						<SidebarTrigger className="-ml-1" />
