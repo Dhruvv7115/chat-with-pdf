6ccb8ab616b1b13bb71a682c19a9c0ada1f3419d
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-08-11 20:59:47 +0530

    fix: added webhooks api route to proxy to check stripe webhook locally

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,11 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-	matcher: ["/dashboard", "/chat", "/settings", "/billings", "/api/:path*"],
+	matcher: [
+		"/dashboard",
+		"/chat",
+		"/settings",
+		"/billings",
+		"/api/((?!webhooks).*)",
+	],
 };
