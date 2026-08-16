735ac154a7050e19eee6eed1b0a227c874198425
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-08-16 23:05:07 +0530

    refactor: remove anydoc-darwin-arm64 and using normal anydoc (added firecrawl/anydoc to serverExternalPackages in next config)

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [new URL("https://lh3.googleusercontent.com/**")],
 	},
+	serverExternalPackages: ["@firecrawl/anydoc"],
 };
 
 export default nextConfig;
