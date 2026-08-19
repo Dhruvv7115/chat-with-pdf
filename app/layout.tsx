2fb5ef9188b76c58e35f58e387e949d82de7f660
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-08-19 19:34:54 +0530

    feat: add font and packages for markdown styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Literata, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+const literata = Literata({
+	subsets: ["latin"],
+	variable: "--font-literata",
+});
+
+const jetBrainsMono = JetBrains_Mono({
+	subsets: ["latin"],
+	variable: "--font-jet-brains-mono",
+});
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -26,12 +36,9 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html
-			lang="en"
-			className={inter.variable}
-		>
+		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`${geistSans.variable} ${geistMono.variable} ${literata.variable} ${jetBrainsMono.variable}  antialiased`}
 			>
 				<Toaster
 					richColors
