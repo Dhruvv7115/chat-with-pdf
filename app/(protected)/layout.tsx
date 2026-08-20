a1048a68b130875a45b118e4100ca8d1de99c6a7
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-08-20 20:03:10 +0530

    refactor: layout changes

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -12,14 +12,13 @@ const layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
-			<SidebarInset className="h-[calc(100vh-1rem)] flex flex-col rounded-xl">
-				<header className="flex h-16 shrink-0 items-center gap-2 border-b border-neutral-200 dark:border-neutral-800">
+			<SidebarInset className="h-[calc(100vh-1rem)] flex flex-col overflow-auto">
+				<header className="flex h-16 shrink-0 items-center gap-2 bg-sidebar">
 					<div className="flex items-center gap-2 px-4">
 						<SidebarTrigger className="-ml-1" />
-						<Separator orientation="vertical" />
 					</div>
 				</header>
-				<div className="flex-1 overflow-scroll rounded-xl">{children}</div>
+				<div className="flex-1 overflow-auto scrollbar-none">{children}</div>
 			</SidebarInset>
 		</SidebarProvider>
 	);
